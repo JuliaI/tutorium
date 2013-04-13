73a688ca3a6b3d3e6cda467bf2fdca1ceb47edfa
JuliaI <julia.ilnseher@gmx.de>
2013-04-13 09:52:45 +0200

    first hello-world-app

---
+++ src/de/juliai/tutorium/Example1Activity.java
@@ -0,0 +1,17 @@
+package de.juliai.tutorium;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author JuliaI
+ */
+public class Example1Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main1);
+	}
+
+}
